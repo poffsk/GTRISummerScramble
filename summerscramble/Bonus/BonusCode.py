095cf9920b342adf1f0ea9046b15f2c8e7e0014d
Sarah Poff <poffsk@gmail.com>
2020-07-19 14:33:41 -0400

    Data created for uni-gram only model

--- summerscramble/Bonus/BonusCode.py
+++ summerscramble/Bonus/BonusCode.py
@@ -10,7 +10,7 @@ Created on Tue Jul  7 18:24:58 2020
  The network topology can be summarized as follows.
  
  
- Convolutional layer with 30 feature maps of size 5×5.
+Convolutional layer with 30 feature maps of size 5×5.
 Pooling layer taking the max over 2*2 patches.
 Convolutional layer with 15 feature maps of size 3×3.
 Pooling layer taking the max over 2*2 patches.
@@ -33,27 +33,83 @@ import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import KFold
 #import foolbox as fb
-from foolbox import TensorFlowModel, accuracy, samples
-from foolbox.attacks import LinfPGD
-from foolbox.attacks import FGSM
-import eagerpy as ep
 import matplotlib.pyplot as plt
+import json
 # from sklearn.model_selection import cross_val_score
 # Final evaluation of the model
 #clf = svm.SVC(kernel='linear', C=1)
 #scores = cross_val_score(clf, iris.data, iris.target, cv=5)
 
+#Extract data
+with open('dict_train.json') as train_file:
+    data = json.load(train_file)
+    
+with open('test_data.json') as test_file:
+    testdata = json.load(test_file)
+    
+
+""">>> data.keys()
+dict_keys(['data', 'row_index', 'column_index', 'schema', 'shape', 'labels'])"""
+
+#15670 instances total
+#3133 will be test, 12537 are examples
+
+datalist = data['data'] #has length of 6317199, range of 1-56 (0 is default), I assume this is counts
+rowlist = data['row_index'] #6317199, (0, 12535)
+collist = data['column_index'] #6317199 (0, 106427)
+col_schema = data['schema'] #106428, 1111 API feature and their combos (1, 2, or 3 at a time)
+shape = data['shape'] #[12536, 106428] = #labels, #schema
+labels = data['labels'] #12536, number of training examples
+
+num_APIs = 0
+for i in range(0, len(col_schema)):
+    strnums = col_schema[i].split()
+    for j in strnums:
+        if int(j) > num_APIs:
+            num_APIs = int(j)
+
+#start with just unigrams
+#make list of old column numbers and map them to new column numbers
+
+maplist=[] #will have ordered pair (oldcolnumber, newcolnumber)
+uni_col_schema = [] #just going to be ['0', '1', ..., '1110']
+#want to go from 106428 columns to 1111 for unigram 
+for i in range(0, len(col_schema)):
+    if len(col_schema[i].split()) == 1: #there is just a unigram
+        maplist.append((i, int(col_schema[i])))
+        uni_col_schema.append(col_schema[i])
+#col_schema[106423] = '1110'
+        
+        
+#Parse down datalist to keep only unigrams
+uni_datalist = [] #will be abridged
+uni_rowlist = [] #will be abridged
+uni_collist = [] #will be mapped from maplist
+#labels will be the same bc we aren't deleting examples, just abridging info
+
+for i in range(0, len(datalist)):
+    for j in range(0, len(maplist)): #is there a way to avoid this second loop? maybe I shouldn't map
+        if collist[i] == maplist[j][0]: #we found a match to keep
+            uni_datalist.append(datalist[i]) #keep that datapoint
+            uni_rowlist.append(rowlist[i]) #keep that row index
+            uni_collist.append(maplist[j][1]) #add the new, abridged column
+
+
 
 kf = KFold(n_splits=2)
+
 
 # load data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 # reshape to be [samples][width][height][channels]
 X_train = X_train.reshape((X_train.shape[0], 28, 28, 1)).astype('float32')
 X_test = X_test.reshape((X_test.shape[0], 28, 28, 1)).astype('float32')
+
 # normalize inputs from 0-255 to 0-1
-X_train = X_train / 255
-X_test = X_test / 255
+X_train = X_train / (num_APIs + 1) #may need to also count test data APIs
+X_test = X_test / (num_APIs + 1) #bc don't want to give value greater than one
+#X_train = X_train / 255
+#X_test = X_test / 255
 # one hot encode outputs
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
@@ -66,13 +122,13 @@ kf.get_n_splits(X_train)
 >>> print(kf)
 KFold(n_splits=2, random_state=None, shuffle=False)"""
 
-
+#let's start with just dense layer connecting 1111 inputs to 5 outputs
 def larger_model():
     model = Sequential()
-    model.add(Conv2D(30, (5, 5), input_shape=(28, 28, 1), activation='relu'))
-    model.add(MaxPooling2D())
-    model.add(Conv2D(15, (3, 3), activation='relu'))
-    model.add(MaxPooling2D())
+    model.add(Conv2D(30, (5, 5), input_shape=(28, 28, 1), activation='relu')) #modify
+    model.add(MaxPooling2D()) #modify
+    model.add(Conv2D(15, (3, 3), activation='relu')) #modify
+    model.add(MaxPooling2D()) #modify?
     model.add(Dropout(0.2))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
@@ -90,89 +146,10 @@ for train_index, test_index in kf.split(X_train): #might need to tack on targets
     X_trainK, X_testK = X_train[train_index], X_train[test_index]
     y_trainK, y_testK = y_train[train_index], y_train[test_index]
     model.fit(X_trainK, y_trainK, validation_data=(X_testK, y_testK), epochs=2, batch_size=100)
-    scores = model.evaluate(X_test, y_test, verbose=0)
+    scores = model.evaluate(X_test, y_test, verbose=0) #should I change this verbose value?
     scoresvec.append((100-scores[1]*100))
 
 #CUDA, total of three
     #cuda 10.1...
 y_pred = model.predict(X_test[0:101])
 #model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
-
-
-#images = X_test[0:101]
-#labels = y_test[0:101]
-
-
-#fmodel = create()
-#fmodel = fb.PyTorchModel(model=model, bounds=(0, 1))
-#return fmodel
-# https://foolbox.readthedocs.io/en/v2.3.0/user/examples.html
-#fmodel = fb.models.TensorFlowModel(model=model, bounds = (0.0,1.0), device = None) #this one crashes
-fmodel = TensorFlowModel(model, bounds = (0,1))
-#nooo it crashed
-
-images, labels = samples(fmodel, dataset = "mnist", batchsize = 100)
-images1, labels1 = ep.astensors(*samples(fmodel, dataset = "mnist", batchsize = 100)) 
-print(accuracy(fmodel, images1, labels1))
-
-
-images_arr = np.array(images)
-#plot images before attack
-fig, axes = plt.subplots(1, 100, figsize = (28,28))
-axes = axes.flatten()
-for img, ax in zip(images_arr, axes):
-    ax.imshow(np.squeeze(img), cmap="gray")
-    ax.axis("off")
-plt.tight_layout()
-plt.show()
-
-
-epsilons = [0.0, 0.001, 0.01, 0.03, 0.1, 0.3, 0.5, 1.0]
-
-
-fmodel = foolbox.models.TensorFlowModel(model, bounds = (0.0,1.0)) #this one crashes
-fmodel = fmodel.transform_bounds((0,1))
-#assert fmodel.bounds==(0,1)
-
-#images = X_test[0:101]
-#labels = y_test[0:101]
-
-#images, labels = ep.astensors(*samples(fmodel, dataset="mnist", batchsize=20))
-images, labels = ep.astensors(*samples(images, labels))
-#print(accuracy(fmodel, images, labels))
-
-
-attack = fb.attacks.LinfPGD()
-_, advs, success = attack(fmodel, images, labels, epsilons= 0.03)
-
-print(success)
-
-
-listsuccess = []
-for anepval in success:
-    print(anepval)
-    totalsuccess=0
-    for abool in anepval:
-        abool1 = np.array(abool)
-        if abool1==True:
-            totalsuccess+=1
-    listsuccess.append(totalsuccess)
-    
-print(listsucess)
-
-
-#with tf.keras.backend.get_session().as_default():
-#    foolbox_model = foolbox.models.TensorFlowModel.from_keras(model=network, bounds=(0.0, 1.0))
-
-# https://foolbox.readthedocs.io/en/v2.3.0/user/examples.html
-
-
-
-predict = fmodel(images).numpy()
-#I wonder what the two lines below do
-tf.nn.softmax(predict).numpy()
-correct_pred = tf.math.argmax(predict, 1)
-print(correct_pred)
-
-
-
